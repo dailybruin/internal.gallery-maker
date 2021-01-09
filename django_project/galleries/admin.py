7ac6d16e60df00df76c40a4fe44a3524e7356e3a
shayan <42shayan42@gmail.com>
2021-01-09 15:18:03 -0800

    added galleries app with models Image and Gallery

---
+++ django_project/galleries/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Image, Gallery
+
+
+
+admin.site.register(Image)
+
+
+admin.site.register(Gallery)
---
+++ django_project/galleries/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models import Model
+
+# Create your models here.
+
+
+class Gallery(models.Model):
+
+
+	# gallery should have id (primary key)
+	#  	 and a gallery_name (textfield)
+
+	name = models.TextField(max_length=50)
+
+
+
+class Image(models.Model):
+
+	img_url = models.URLField(max_length=200)
+	name = models.TextField(max_length=50)
+
+	# foregin key
+	gallery_id = models.ForeignKey(Gallery, on_delete=models.CASCADE, null=True )
+
+
+
+	# 
+	#
+	#
+	#
+
+
