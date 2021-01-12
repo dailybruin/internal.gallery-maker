30145a785d896e0a433c7bd9316b87d06ce3ccec
Lisha Mohan <lisha4mohan@yahoo.com>
2021-01-11 19:48:13 -0800

    added react router

---
+++ react/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import {Switch, Route} from "react-router-dom"
+import Home from './Home'
+import CreateUpdateGallery from './CreateUpdateGallery'
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/create" component={CreateUpdateGallery}/>
+        <Route path="/update/:id" component={CreateUpdateGallery}/>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
