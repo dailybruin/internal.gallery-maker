08921e21ecbda6fc23771880f4378010fd7e9e6a
shayan <42shayan42@gmail.com>
2021-01-29 15:45:33 -0800

    fixed serializers, models, and made sure url endpoints worked¨

--- django_project/galleries/models.py
+++ django_project/galleries/models.py
@@ -7,37 +7,43 @@ from django.db.models import Model
 class Gallery(models.Model):
 
 
-	# gallery should have id (primary key)
-	#  	 and a gallery_name (textfield)
+    # gallery should have id (primary key)
+    #       and a gallery_name (textfield)
 
-	description = models.TextField(max_length=50)
+    ALT= 'alt'
+    NOTALT = 'notalt'
 
-	credits = models.TextField(max_length=50, null=True)
-
-	ALT= 'alt'
-	NOTALT = 'notalt'
-
-	POSSIBLE_VIEW_CHOICES = [
+    POSSIBLE_VIEW_CHOICES = [
 
     (ALT, 'alternating'),
     (NOTALT, 'not-alternating')
 
-	]
+    ]
 
-	view_choices = models.CharField(
-		max_length=10,
-		choices=POSSIBLE_VIEW_CHOICES,
-		default = ALT
-	)
+    layout = models.CharField(
+        max_length=10,
+        choices=POSSIBLE_VIEW_CHOICES,
+        default = ALT
+    )
 
 
 
 
 class Image(models.Model):
+    # image url
+    img_url = models.URLField(max_length=200)
+    # desc of img
+    description = models.TextField(max_length=300, default="")
+    # photo creds
+    credits = models.TextField(max_length=50, null=True)
 
-	img_url = models.URLField(max_length=200)
-	description = models.TextField(max_length=300, default="")
+    # specify order of imgs
+    index = models.IntegerField()
 
-	# foregin key
-	gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE, related_name="images")
+    # foregin key
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE, related_name="images")
+
+    class Meta:
+        ordering = ['index']
+
 
