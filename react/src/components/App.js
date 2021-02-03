a9ed2a195ed8333995a85eed19a0c5c6ec7dd716
Lisha Mohan <lisha4mohan@yahoo.com>
2021-02-02 17:04:41 -0800

    Revert "cors test code"
    This reverts commit c5027447b87cc83dd66690abaf72dcd07a47f86f.

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -3,21 +3,8 @@ import './App.css';
 import {Switch, Route} from "react-router-dom"
 import Home from './Home'
 import CreateUpdateGallery from './CreateUpdateGallery'
-import {useEffect} from 'react';
-import axios from 'axios';
-//https://gallery.dailybruin.com/django/get_gallery_data/1
 
 function App() {
-  useEffect(() => {
-    axios.get(`http://localhost:8000/django/get_gallery_data/1`)
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }, []);
-
   return (
     <div className="App">
       <Switch>
