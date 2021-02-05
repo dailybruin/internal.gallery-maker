c2418dda3c601b6337f15ea5d68e5647695abd65
shayan <42shayan42@gmail.com>
2021-02-04 22:20:18 -0800

    fixed models and serializers to have less fields for mainsite endpoint

--- django_project/galleries/serializers.py
+++ django_project/galleries/serializers.py
@@ -7,7 +7,26 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = ['img_url', 'description', 'credits', 'index', 'gallery', 'id']
 
 class GallerySerializer(serializers.ModelSerializer):
+
     images = ImageSerializer(many=True, read_only=True)
     class Meta:
         model = Gallery
         fields = ['layout', 'images', 'id']
+
+
+
+class MainSiteImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['img_url', 'description', 'credits',]
+
+class MainSiteGallerySerializer(serializers.ModelSerializer):
+    def get_images_as_data(self, obj):
+        return MainSiteImageSerializer(obj.images.all(), many=True).data
+
+    data = serializers.SerializerMethodField('get_images_as_data')
+
+    class Meta:
+        model = Gallery
+        fields = ['layout', 'data']
+
