13ea3edcaa284247d04449607811bbfcce3d6bac
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-02-18 12:11:44 -0800

    Rearrange Images scaffolding

--- react/src/components/Home/index.js
+++ react/src/components/Home/index.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import RearrangeImages from '../RearrangeImages';
 
 function Home(props) {
-    return (
-        <div>
-            <div>Home Page</div>
-            <div>
-                <Link to='/create'>Create</Link>
-            </div>
-            <div>
-                <Link to='/update/1'>Update</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>Home Page</div>
+      <div>
+        <Link to="/create">Create</Link>
+      </div>
+      <div>
+        <Link to="/update/1">Update</Link>
+      </div>
+      <RearrangeImages />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
