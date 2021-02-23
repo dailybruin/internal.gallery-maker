45943d50d6c7cf35e999a6b847662b4e38042926
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-02-23 13:53:57 -0800

    * Updated state to work with redux. * Added delete gallery to redux.

--- react/src/components/RearrangeImages/RearrangeImages.js
+++ react/src/components/RearrangeImages/RearrangeImages.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
-import update from 'immutability-helper';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ImageList from './ImageList';
 import { isTouchDevice } from './utils';
@@ -12,38 +12,66 @@ import './RearrangeImages.css';
 const backendForDND = isTouchDevice() ? TouchBackend : HTML5Backend;
 
 function RearrangeImages() {
-  const [images, setImages] = useState([
-    {
-      url:
-        'https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg',
-      id: 1,
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      id: 2,
-    },
-  ]);
+  const reduxGallery = useSelector((state) => state.editGallery.gallery); // for retrieving state
+  const dispatch = useDispatch(); // for changing state
+
+  console.log(reduxGallery);
+  const addImage = () => {
+    console.log('Adding entry');
+    dispatch({
+      type: 'EDIT_GALLERY',
+      payload: [
+        ...reduxGallery,
+        {
+          url: 'https://redux.js.org/img/redux.svg',
+          caption: 'Redux',
+        },
+      ],
+    });
+  };
+
+  const addfirstcat = () => {
+    console.log('Adding entry');
+    dispatch({
+      type: 'EDIT_GALLERY',
+      payload: [
+        ...reduxGallery,
+        {
+          url:
+            'https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg',
+          caption: 'Redux',
+        },
+      ],
+    });
+  };
 
   const moveImage = (dragIndex, hoverIndex) => {
-    const draggedImage = images[dragIndex];
-    setImages(
-      update(images, {
-        $splice: [
-          [dragIndex, 1],
-          [hoverIndex, 0, draggedImage],
-        ],
-      })
-    );
+    const draggedImage = reduxGallery[dragIndex];
+    let rearrangedGallery = [...reduxGallery];
+    rearrangedGallery.splice(dragIndex, 1);
+    rearrangedGallery.splice(hoverIndex, 0, draggedImage);
+    dispatch({
+      type: 'EDIT_GALLERY',
+      payload: rearrangedGallery,
+    });
+  };
+
+  const resetGallery = () => {
+    dispatch({
+      type: 'DELETE_GALLERY',
+    });
   };
   return (
     <div className="rearrange-images-container">
-      <h1 className="text-center">Drag and Drop Example</h1>
-      {images && images.length > 0 && (
+      <h1 className="text-center">Rearrange Images</h1>
+      <button onClick={addImage}> add to fake gallery </button>
+      <button onClick={addfirstcat}> add first cat </button>
+      <button onClick={resetGallery}>delete gallery</button>
+      {reduxGallery && reduxGallery.length > 0 && (
         <h3 className="text-center">Drag the Images to change positions</h3>
       )}
       <DndProvider backend={backendForDND}>
-        <ImageList images={images} moveImage={moveImage} />
+        <ImageList images={reduxGallery} moveImage={moveImage} />
       </DndProvider>
     </div>
   );
