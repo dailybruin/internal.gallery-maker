abecd12bed1129ddea79a624b0338c44de35df2d
“aritramullick” <aritramullick@g.ucla.edu>
2021-03-02 04:43:09 +0400

    Added final features with username

--- django_project/oauth/views.py
+++ django_project/oauth/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 import requests
 import json
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 import os
 import environ
 from django.contrib.sessions.models import Session
@@ -13,14 +13,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def slack_auth(request):
-    logging.debug(request)
-    logging.debug(request.GET)
+    # logging.debug(request)
+    # logging.debug(request.GET)
     API_ENDPOINT = "https://slack.com/api/oauth.v2.access"
     # your API key here 
     code = request.GET['code']
-    logging.debug(code)
+    # logging.debug(code)
     # data to be sent to api 
-    logging.debug(SLACK_SECRET_KEY)
+    # logging.debug(SLACK_SECRET_KEY)
     data = {
         'client_id':'4526132454.1654017102375',
         'client_secret':SLACK_SECRET_KEY,
@@ -28,52 +28,81 @@ def slack_auth(request):
         'redirect_uri':'http://localhost:8000/django/oauth/slack_auth' 
         } 
     # sending post request and saving response as response object 
-    r = requests.post(url = API_ENDPOINT, data = data) 
-    logging.debug("Request Body", r.request.body)
-    logging.debug("Request", r.request)
-    logging.debug("Request headers", r.request.headers)
-    logging.debug("Request text", r.text)
-    jsoned = json.loads(r.text)
-    logging.debug(jsoned["ok"])
-    loggedIn = jsoned["ok"]
-    userId = jsoned["authed_user"]["id"]
-    logging.debug('userId', userId)
-    if loggedIn:
-        user = authenticate(username=userId,password='')
-        logging.debug('Request', request)
-        logging.debug('user', user)
-        instance = User.objects.all()
-        logging.debug('instance', instance)
-        # sessions = Session.objects.all()
-        # sessions.delete()
-        # instance.delete()
-        if user is not None:
-            # The user already exists in the database
-            # return HttpResponse("Success with an existing user")
-            login(request, user)
-            return redirect('/')
-        else:
-            # A new user needs to be made
-            new_user = User.objects.create_user(username=userId, password='')
-            new_user.is_active = True
-            new_user.save()
-            new_user = authenticate(username=userId, password='')
-            login(request, new_user)
-            # return HttpResponse("Success after making a new user")
-            return redirect('/')
-    if not loggedIn:
+    r = "Checking"
+    try:
+        r = requests.post(url = API_ENDPOINT, data = data) 
+    except:
         return JsonResponse({
-            "error": "error communicating with Slack API"
+            "error": "error communicating with POST request for auth token"
         }, safe=False)
+    finally:
+        print(type(r))
+        
+        # logging.debug("Request Body", r.request.body)
+        # logging.debug("Request", r.request)
+        # logging.debug("Request headers", r.request.headers)
+        # logging.debug("Request text", r.text)
+        jsoned = json.loads(r.text)
+        # logging.debug(jsoned["ok"])
+        loggedIn = jsoned["ok"]
+        userId = jsoned["authed_user"]["id"]
+        accessToken = jsoned["authed_user"]["access_token"]
+        data = {
+            "token" : accessToken
+        }
+        API_ENDPOINT = "https://slack.com/api/users.identity"
+        r2 = "trying a request"
+        try:
+            r2 = requests.post(url = API_ENDPOINT, data = data) 
+        except:
+            return JsonResponse({
+            "error": "error communicating with POST request for username"
+        }, safe=False)
+        finally:
+            finding_name = json.loads(r2.text)
+            name = finding_name["user"]["name"]
+            # print(name)
+            # logging.debug('userId', userId)
+            if loggedIn:
+                userId = str(userId)
+                user = authenticate(username=userId,password='', first_name=name)
+                # logging.debug('Request', request)
+                # logging.debug('user', user)
+                instance = User.objects.all()
+                # logging.debug('instance', instance)
+                # sessions = Session.objects.all()
+                # sessions.delete()
+                # instance.delete()
+                if user is not None:
+                    # The user already exists in the database
+                    
+                    login(request, user)
+                    # print(user)
+                    return redirect('/')
+                else:
+                    # A new user needs to be made
+                    new_user = User.objects.create_user(username=userId, password='', first_name=name)
+                    new_user.is_active = True
+                    new_user.save()
+                    new_user = authenticate(username=userId, password='',first_name=name)
+                    login(request, new_user)
+                    # return HttpResponse("Success after making a new user")
+                    return redirect('/')
+            if not loggedIn:
+                return JsonResponse({
+                    "error": "error communicating with Slack API"
+                }, safe=False)
 
 def get_logged_in(request):
-    logging.debug("request.user authenticate", request.user.is_authenticated)
-    logging.debug('request.user', request.user)
+    # logging.debug("request.user authenticate", request.user.is_authenticated)
+    # logging.debug('request.user', request.user)
     sessions = Session.objects.all()
-    logging.debug('sessions', sessions )
+    # logging.debug('sessions', sessions )
     instance = User.objects.all()
-    logging.debug('instance', instance)
+    # instance.delete()
+    # logging.debug('instance', instance)
     if request.user.is_authenticated:
+        # logout(request)
         return HttpResponse(True)
     else:
         return HttpResponse(False)
