ed6811f233573721ecd4f5a5c67025d578ee1f6f
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-03-01 19:12:08 -0800

    Removed cat debugging.

--- react/src/components/RearrangeImages/RearrangeImages.js
+++ react/src/components/RearrangeImages/RearrangeImages.js
@@ -17,34 +17,34 @@ function RearrangeImages() {
   const dispatch = useDispatch(); // for changing state
   // console.log(reduxGallery);
 
-  const addfirstcat = () => {
-    console.log('Adding entry');
-    dispatch({
-      type: 'EDIT_GALLERY',
-      payload: [
-        ...reduxGallery,
-        {
-          url:
-            'https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg',
-          caption: 'Redux',
-        },
-      ],
-    });
-  };
+  // const addfirstcat = () => {
+  //   console.log('Adding entry');
+  //   dispatch({
+  //     type: 'EDIT_GALLERY',
+  //     payload: [
+  //       ...reduxGallery,
+  //       {
+  //         url:
+  //           'https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg',
+  //         caption: 'Redux',
+  //       },
+  //     ],
+  //   });
+  // };
 
-  const addsecondcat = () => {
-    dispatch({
-      type: 'EDIT_GALLERY',
-      payload: [
-        ...reduxGallery,
-        {
-          url:
-            'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-          caption: 'Redux',
-        },
-      ],
-    });
-  };
+  // const addsecondcat = () => {
+  //   dispatch({
+  //     type: 'EDIT_GALLERY',
+  //     payload: [
+  //       ...reduxGallery,
+  //       {
+  //         url:
+  //           'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  //         caption: 'Redux',
+  //       },
+  //     ],
+  //   });
+  // };
 
   const resetGallery = () => {
     dispatch({
@@ -68,9 +68,9 @@ function RearrangeImages() {
 
   return (
     <div>
-      <button onClick={addfirstcat}> add first cat </button>
+      {/* <button onClick={addfirstcat}> add first cat </button>
       <button onClick={addsecondcat}> add second cat </button>
-      <button onClick={resetGallery}>delete gallery</button>
+      <button onClick={resetGallery}>delete gallery</button> */}
       {reduxGallery && reduxGallery.length > 0 && (
         <div className="rearrange-images-container">
           <h3 className="text-center">Drag to rearrange images</h3>
