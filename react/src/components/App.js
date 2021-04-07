1198fdd52091d6e979c3b7b2ec616e4666d88b90
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-04-07 15:53:04 -0700

    Added login protection to all routes

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -6,6 +6,7 @@ import CreateUpdateGallery from './CreateUpdateGallery';
 import Signup from './Signup';
 import { RearrangeImages } from './RearrangeImages';
 import { API_ROOT } from '../constants/api';
+import { LoginWrapper } from '../components/LoginWrapper';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -14,14 +15,12 @@ function App() {
     getAuthentication();
   });
   const getAuthentication = async () => {
-    let response = await fetch(
-      `${API_ROOT}/oauth/get_logged_in`
-    );
+    let response = await fetch(`${API_ROOT}/oauth/get_logged_in`);
     // console.log('response', response);
     let text = await response.text();
     text = JSON.parse(text);
-    console.log('response text', text["ok"]);
-    if (text["ok"]) {
+    console.log('response text', text['ok']);
+    if (text['ok']) {
       setLoggedIn(true);
       setLoading(false);
       console.log('set to true');
@@ -33,15 +32,38 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/create" component={CreateUpdateGallery} />
+        {/* <Route path="/create" component={CreateUpdateGallery} />
         <Route path="/update/:id" component={CreateUpdateGallery} />
+        <Route path="/rearrange" component={RearrangeImages} /> */}
         <Route path="/signup" component={Signup} />
-        <Route path="/rearrange" component={RearrangeImages} />
-        <Route
+        <LoginWrapper
+          path="/create"
+          component={CreateUpdateGallery}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        <LoginWrapper
+          path="/update/:id"
+          component={CreateUpdateGallery}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        <LoginWrapper
+          path="/rearrange"
+          component={RearrangeImages}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        <LoginWrapper
+          path="/"
+          component={Home}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        {/* <Route
           path="/"
           render={() => <Home isLoggedIn={loggedIn} isLoading={isLoading} />}
-        />
-        
+        /> */}
       </Switch>
     </div>
   );
--- react/src/components/Home/index.js
+++ react/src/components/Home/index.js
@@ -6,10 +6,10 @@ import { Button } from 'antd';
 import classes from './index.module.css';
 import GalleryList from './gallery-list';
 import SubmitButton from '../SubmitButton';
-import GalleryBasicInfo from "../GalleryBasicInfo"
+import GalleryBasicInfo from '../GalleryBasicInfo';
 
 function Home(props) {
-  return props.isLoggedIn ? (
+  return (
     <div>
       <h1>Galleries</h1>
       <div className={classes.Container}>
@@ -18,21 +18,10 @@ function Home(props) {
       <Button type="primary">
         <Link to="/create">New Gallery</Link>
       </Button>
-      {/*<div>
-        <Link to="/rearrange">Rearrange Images</Link>
-      </div>*/}
-      {/*<GalleryBasicInfo/>*/}
-      {/* <div>
-        <SubmitButton /> add id when updating, if creating ignore
-      </div> */}
       <div>
         <Link to="/signup">Signup</Link>
       </div>
     </div>
-  ) : props.isLoading ? (
-    <div>Loading</div>
-  ) : (
-    <Redirect to="/signup" />
   );
 }
 
