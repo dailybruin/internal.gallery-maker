50e9ca6b2f9c1ac9117e2cd4ef815e15d6091f97
“aritramullick” <aritramullick@g.ucla.edu>
2021-04-12 17:30:52 -0700

    Production changes

--- react/src/components/Signup/index.js
+++ react/src/components/Signup/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import SlackLogin from 'react-slack-login';
 import './index.css';
+import {API_ROOT} from '../../constants/api';
 
 function Signup() {
+  let redirect_uri = API_ROOT+"/oauth/slack_auth";
   return (
     <div className="signup-container">
       {/* <a href={url}>Do things.</a> */}
       <div className="slack-link-container">
-        <a href="https://slack.com/oauth/v2/authorize?user_scope=identity.basic&client_id=4526132454.1654017102375">
+        <a href={"https://slack.com/oauth/v2/authorize?user_scope=identity.basic&client_id=4526132454.1973927489168&redirect_uri="+ redirect_uri + "&state="+redirect_uri}>
           <img
             alt="Sign in with Slack"
             height="40"
