7bdd63515b0b2632ca4ae95daceec77fb03518c8
aritramullick <aritramullick@g.ucla.edu>
2021-04-23 18:56:37 -0700

    Merge changes

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -7,6 +7,7 @@ import Signup from './Signup';
 import { RearrangeImages } from './RearrangeImages';
 import UploadToWordpress from './UploadToWordpress';
 import { API_ROOT } from '../constants/api';
+import { LoginWrapper } from '../components/LoginWrapper';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -15,14 +16,16 @@ function App() {
     getAuthentication();
   });
   const getAuthentication = async () => {
-    let response = await fetch(
-      `${API_ROOT}/oauth/get_logged_in`
-    );
+    let response = await fetch(`${API_ROOT}/oauth/get_logged_in`);
     // console.log('response', response);
     let text = await response.text();
     text = JSON.parse(text);
-    console.log('response text', text["ok"]);
-    if (text["ok"]) {
+    console.log('response text', text['ok']);
+    console.log('response.ok', response.ok);
+    console.log('text', text);
+    let worked = text['ok'] == 'False' ? false : true;
+    console.log('worked', worked);
+    if (worked) {
       setLoggedIn(true);
       setLoading(false);
       console.log('set to true');
@@ -34,16 +37,36 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/create" component={CreateUpdateGallery} />
+        {/* <Route path="/create" component={CreateUpdateGallery} />
         <Route path="/update/:id" component={CreateUpdateGallery} />
+        <Route path="/rearrange" component={RearrangeImages} /> */}
         <Route path="/signup" component={Signup} />
+        <LoginWrapper
+          path="/create"
+          component={CreateUpdateGallery}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        <LoginWrapper
+          path="/update/:id"
+          component={CreateUpdateGallery}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        <LoginWrapper
+          path="/rearrange"
+          component={RearrangeImages}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
+        <LoginWrapper
+          path="/"
+          component={Home}
+          isLoggedIn={loggedIn}
+          isLoading={isLoading}
+        />
         <Route path="/rearrange" component={RearrangeImages} />
         <Route path="/upload" component={UploadToWordpress} />
-        <Route
-          path="/"
-          render={() => <Home isLoggedIn={loggedIn} isLoading={isLoading} />}
-        />
-        
       </Switch>
     </div>
   );
